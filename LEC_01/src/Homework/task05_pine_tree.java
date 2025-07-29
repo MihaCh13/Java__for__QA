47f4ae3292e8155170c31a3d89faa72deabd82ee
Miha_Ch <148248932+MihaCh13@users.noreply.github.com>
2025-07-29 20:57:56 +0300

    Add LEC_01 project files

---
+++ LEC_01/src/Homework/task05_pine_tree.java
@@ -0,0 +1,18 @@
+package Homework;
+
+public class task05_pine_tree {
+    public static void main(String[] args) {
+        System.out.println("     *");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * *");
+        System.out.println("    **");
+        System.out.println("    **");
+        System.out.println("    **");
+
+    }
+}
